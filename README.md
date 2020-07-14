381340388d43605262def78b4d112302440e0ddf
Srikrishna Sekhar <srikrishna@idia.ac.za>
2020-07-14 23:05:38 +0200

    Parallactic angle calculation script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # astroscripts
-Scripts that are potentially useful (and poorly documented)
+
+Scripts that are potentially useful (and poorly documented). This is the place
+where I put script that might be more generally useful.
+
+They have no guarantee of working outside their intended purpose.
+
+I hold no responsibility for your actions. 
+
+Here be dragons.
---
+++ print_parang.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+import click
+
+@click.command()
+@click.argument('name', type=str)
+@click.argument('beg_time', type=str)
+@click.argument('end_time', type=str)
+@click.argument('coord_ra', type=str)
+@click.argument('coord_dec', type=str)
+@click.option('--timezone', default='Etc/GMT0',
+              help='Time zone (default: Etc/GMT0)')
+def print_parangs(name, beg_time, end_time, coord_ra, coord_dec, timezone):
+    """
+    Prints the parallactic angle range given the start and end observing
+    times (in UCT/GMT), the telescope name and the celestial target
+    coordinates.
+
+    Currently accepted telescope names : uGMRT or MeerKAT (case insensitive)
+
+    The beg time and end time should be in the format '2020-01-01T00:00:00'
+
+    The target coordinates must be in HMS DMS (i.e., 00h00m00s +00d00m00s)
+    """
+
+    from astroplan import Observer
+    import astropy.units as u
+    from astropy.coordinates import SkyCoord
+    from astropy.time import Time
+
+    import numpy as np
+
+    if name.lower() == 'ugmrt' or name.lower() == 'gmrt':
+        obs = Observer(longitude = '74d02m59s' , latitude = '19d05m47s' ,
+                       elevation = 0*u.m, name = 'uGMRT', timezone=timezone)
+    elif name.lower() == 'meerkat':
+        obs = Observer(longitude = '21d24m40s' , latitude='-30d43m16s',
+                       elevation = 0*u.m , name = 'MeerKAT',
+                       timezone=timezone)
+    else:
+        raise NotImplementedError("Unknown telescope name")
+
+
+    coord = SkyCoord(coord_ra, coord_dec)
+
+    beg_time = Time(beg_time)
+    end_time = Time(end_time)
+
+    parang_beg = np.rad2deg(obs.parallactic_angle(beg_time, coord))
+    parang_end = np.rad2deg(obs.parallactic_angle(end_time, coord))
+
+    print("Beginning parallactic angle {:.3f}".format(parang_beg))
+    print("Ending parallactic angle {:.3f}".format(parang_end.deg))
+
+    print("Parang delta {:.3f}".format(parang_end - parang_beg))
+
+if __name__ == '__main__':
+    print_parangs()
