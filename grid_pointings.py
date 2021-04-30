a2b5eee0073d009b9623298f17baf62b58632cf9
Srikrishna Sekhar <srikrishna@idia.ac.za>
2021-05-01 01:35:17 +0530

    Updated for VLA OPT
    And for spherical geometry

--- grid_pointings.py
+++ grid_pointings.py
@@ -8,14 +8,32 @@ from astropy.coordinates import Angle
 import matplotlib.pyplot as plt
 
 
+def get_angle_pieces(angle, which):
+    if which == 'ra':
+        a = int(np.floor(angle.hour))
+        delta = angle.hour - a
+    elif which == 'dec':
+        a = int(np.floor(angle.deg))
+        delta = angle.deg - a
+    else:
+        raise ValueError("Invalid value for 'which'")
+
+    m = int(np.floor(delta * 60.))
+    delta = delta*60. - m
+    s = delta * 60.
+
+    return a,m,s
+
+
 @click.command()
 @click.argument('target_ra')
 @click.argument('target_dec')
 @click.argument('npoint', type=int)
 @click.argument('sep', type=float)
-@click.option('--katopt', is_flag=True, help='Print out source list in a form KAT OPT can understand')
+@click.option('--opt', nargs=1, type=click.Choice(['VLA', 'KAT'], case_sensitive=False))
 @click.option('--srcprefix', type=str, default='', help='Prefix for source names in grid')
-def grid_pointings(target_ra, target_dec, npoint, sep, katopt, srcprefix):
+@click.option('--groupid', type=str, default=None, help='Group ID to use for --opt=VLA')
+def grid_pointings(target_ra, target_dec, npoint, sep, opt, srcprefix, groupid):
     """
     Script to generate the pointing positions to sample an NxN grid around a
     given target location.
@@ -31,6 +49,12 @@ def grid_pointings(target_ra, target_dec, npoint, sep, katopt, srcprefix):
     WARNING : This script uses the *linear* approximation to angle offsets, so
     it will start accumulating large errors for large offsets.
     """
+
+    if opt is not None:
+        opt = opt.lower()
+
+    if opt == 'vla' and groupid is None:
+        raise ValueError("Need a valid group ID")
 
     # Check that the coordinates are alright. If the coordinates are specified in decimal radians it will
     # still work, but that is up to the user to get right.
@@ -56,33 +80,37 @@ def grid_pointings(target_ra, target_dec, npoint, sep, katopt, srcprefix):
         for dd in range(npoint):
             ddec = Angle((dd - halfn) * sep, unit='arcmin')
 
-            new_ra = ph_centre.ra - dra
             new_dec = ph_centre.dec - ddec
+            avg_dec = (ph_centre.dec.degree + new_dec.degree)/2.
+            # Account for spherical change
+            new_ra = (ph_centre.ra - dra) * np.cos(np.deg2rad(avg_dec))
 
             new_pos = SkyCoord(new_ra, new_dec)
             new_pos_list.append(new_pos)
 
-    if katopt:
+    if opt == 'kat':
         ffile = open('katopt.csv', 'w')
         ffile.write('# name, tags, ra, dec (J2000)\n')
         for idx, nn in enumerate(new_pos_list):
             ffile.write('%s%03d, ' % (srcprefix, idx))
             ffile.write('radec target, ')
 
-            rh = int(np.floor(nn.ra.hour))
-            delta = nn.ra.hour - rh
-            rm = int(np.floor(delta * 60.))
-            delta = delta*60. - rm
-            rs = delta * 60.
-
-            dd = int(np.floor(nn.dec.deg))
-            delta = nn.dec.deg - dd
-            dm = int(np.floor(delta * 60.))
-            delta = delta*60 - dm
-            ds = delta*60.
+            rh, rm, rs = get_angle_pieces(nn.ra, 'ra')
+            dd, dm, ds = get_angle_pieces(nn.dec, 'dec')
 
             ffile.write('%02d:%02d:%.2f, %02d:%02d:%.2f\n' % (rh, rm, rs, dd, dm, ds))
         ffile.close()
+    elif opt == 'vla':
+        ffile = open('vlaopt.csv', 'w')
+        for idx, nn in enumerate(new_pos_list):
+
+            rh, rm, rs = get_angle_pieces(nn.ra, 'ra')
+            dd, dm, ds = get_angle_pieces(nn.dec, 'dec')
+
+            ffile.write('%s%03d;%s;' % (srcprefix, idx, groupid))
+            ffile.write('Equatorial;J2000;')
+            ffile.write('%02d:%02d:%.2f;%02d:%02d:%.2f;' % (rh, rm, rs, dd, dm, ds))
+            ffile.write('LSRK;Radio;0.0; ;\n')
     else:
         for nn in new_pos_list:
                 print(nn.to_string('hmsdms'))
